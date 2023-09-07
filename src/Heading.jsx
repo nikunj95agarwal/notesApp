c883ecbdfe0bbfd994442cbc839123e6b9cfcc92
NIKUNJ AGARWAL <nikunj95agarwal@gmail.com>
2023-09-08 00:46:05 +0530

    my first commit

---
+++ src/Heading.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Heading.css";
+
+function Heading() {
+  
+
+  return (
+    <>
+      <div className="heading_div">
+        <h1>NOTES HUB</h1>
+        
+      </div>
+    </>
+  );
+}
+
+export default Heading;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+
+ReactDOM.render(<>
+<Heading/>
+</>, document.getElementById("root"));
