dcc4bf4427ae95566463869baac287e4b5b77cc2
NIKUNJ AGARWAL <133116644+nikunj95agarwal@users.noreply.github.com>
2023-09-08 13:41:10 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import { Routes, Route } from "react-router-dom";
 import Heading from "./Heading";
 import Preloader from "./Preloader";
+// npm install react-router-dom --save
 // import Navbar from "./Navbar";
 import Upload from "./Upload"; // Import your Upload component here
-
+import "./App.css"
 function App() {
   return (
     <>
