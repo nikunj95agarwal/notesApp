e76de7bd565ec37bf500cb2dc471ceb278f87c50
NIKUNJ AGARWAL <133116644+nikunj95agarwal@users.noreply.github.com>
2023-09-08 13:05:02 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App.js';
+<script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
 
 ReactDOM.render(<>
 <App/>
